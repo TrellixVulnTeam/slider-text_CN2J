bdd305a3cbd6e193c0847f5d4ca556c17f5a2252
Solpio <ssusolpio@gmail.com>
2021-08-05 17:26:15 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const slider = document.querySelector('.slider');
+const slider_left = document.querySelector('.slider__left');
+const slider_right = document.querySelector('.slider__right');
+const button_down = document.querySelector('.slider__button_down');
+const button_up = document.querySelector('.slider__button_up');
+const slider_length = slider_left.querySelectorAll('div').length;
+const slider_length1 = slider_right.querySelectorAll('div').length;
+
+
+let activeSlideIndex = 0
+
+
+slider_left.style.top = `-${(slider_length - 1) * 100}vh`;
+
+button_up.addEventListener('click', () => changeSlide('up'))
+button_down.addEventListener('click', () => changeSlide('down'))
+
+
+const changeSlide = (direction) => {
+    const sliderHeight = slider.clientHeight
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slider_length - 1) {
+            activeSlideIndex = 0
+        }
+    }else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slider_length - 1
+        }
+    }
+
+    slider_left.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+    slider_right.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+}
+
+
+
+console.log(slider_length);
